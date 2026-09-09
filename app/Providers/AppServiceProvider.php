1e3be8f7bae62b3c355a5bd7c8e9d0dc204e0648
Devang <vagheladevang123@gmail.com>
2026-09-09 18:08:35 +0530

    fix password length problame

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -44,12 +44,12 @@ class AppServiceProvider extends ServiceProvider
         );
 
         Password::defaults(fn (): ?Password => app()->isProduction()
-            ? Password::min(12)
+            ? Password::min(8)
                 ->mixedCase()
                 ->letters()
                 ->numbers()
                 ->symbols()
-                ->uncompromised()
+                //->uncompromised()
             : null,
         );
     }
